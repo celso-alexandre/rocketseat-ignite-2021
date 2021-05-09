c0844beb0bc8fa975bef54c513004a9479cc5f01
Celso Alexandre <celsoalexandre@live.com>
2021-05-09 15:18:11 -0300

    react: 04-dashgo: 4.9: NavBar functionality

--- reactjs/04-dashgo/src/components/SideBar/NavLink.tsx
+++ reactjs/04-dashgo/src/components/SideBar/NavLink.tsx
@@ -1,8 +1,8 @@
 import { ElementType } from 'react';
 import {
-  Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps,
+  Icon, Text, LinkProps as ChakraLinkProps, Link as ChakraLink,
 } from '@chakra-ui/react';
-import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
 
 interface Props extends ChakraLinkProps {
   icon: ElementType;
@@ -16,11 +16,11 @@ export function NavLink({
   ...rest
 }: Props) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" align="center" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">{children}</Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
